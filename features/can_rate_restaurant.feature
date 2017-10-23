216fd20698d9983d063183b04747a86904862f6c
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-10-23 10:08:19 +0200

    updated steps, removed starts test

--- features/can_rate_restaurant.feature
+++ features/can_rate_restaurant.feature
@@ -22,6 +22,8 @@ Feature: User can rate restaurant
 
   Scenario: User can rate restaurant on scale 1-5
     Given I click "Add Review"
-    Then show me the page
-    Then I click "4" stars
-    And I add comment "Decent restaurant"
+#    Then I click "good" stars
+    And I add comment "Awful restaurant"
+    And I click "Create Review"
+    Then I should see "Awful restaurant"
+#    And I should see the picture "/assets/star-on.png"
--- features/step_definitions/rate_restaurant_steps.rb
+++ features/step_definitions/rate_restaurant_steps.rb
@@ -3,10 +3,13 @@ Given("I give Yappi a rating of {int}") do |int|
 end
 
 Given("I add comment {string}") do |string|
-  fill_in('Comment', :with => 'decent restaurant')
+  fill_in('Comment', with: string)
 end
 
-Then("I click {string} stars") do |string|
-  #find("img[@alt='4']").click
-  page.execute_script("raty('5')")
+Then("I click {string} stars") do |alt|
+  find("img[@alt='4']").click
+  # find('img.star-off.png').click
+  # find("img[alt='good']").click
+  # page.execute_script("raty('5')")
+  # find(:xpath, "//img[@alt='#{alt}']/..").click
 end
